6a723cbc35b2a9db0b1bd2836f04c2487591ceca
Mariana Mariano Borges <mariannamarianoborges@gmail.com>
2024-10-07 21:54:57 -0300

    feat: add project configs

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <Button>olá</Button>
+    </h1>
+  )
+}
+
+export default App
