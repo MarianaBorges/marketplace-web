e6fce5eb02fd61a84b4d791957e3e986491ac902
Mariana Mariano Borges <mariannamarianoborges@gmail.com>
2024-10-12 21:25:24 -0300

    feat: config query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster"
+import { queryClient } from "./lib/react-query";
 function App() {
 
   return (
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   )
 }
 
