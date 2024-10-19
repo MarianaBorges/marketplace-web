e7baa7b5aa7a4cb6a8f1be81767b279820deb7a7
Mariana Mariano Borges <mariannamarianoborges@gmail.com>
2024-10-19 16:06:00 -0300

    feat: add first style to header

--- src/_layout/app.tsx
+++ src/_layout/app.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/header";
 import { Outlet } from "react-router-dom";
 
 export default function AppLayout () {
   return (
     <>
+      <Header></Header>
       <h1>App layout</h1>
       <Outlet />
     </>
